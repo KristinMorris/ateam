ae7e2d630d670cc4b26ef2ab4af7c4d26cbad43a
George Lee <george_bm@aol.com>
2018-02-15 19:28:04 +0000

    george created an input loop

--- Project_Ateam.rb
+++ Project_Ateam.rb
@@ -14,7 +14,31 @@ def space5
   5.times {puts ""}
 end
 
+def add_another_loop
+  sentence = []
+  loop do
+    puts "Add an E-mail"
+    puts "y: Yes"
+    puts "e: Exit"
+    puts "Enter your choice: "
+    answer = gets.chomp.downcase
+    case answer
+    when 'y'
+      puts "Add your Text, one word at a time"
+      string = gets.chomp.downcase
+      sentence.push(string)
+    when 'e'
+      sentence.each do |value|
+        puts "The email is #{value}"
+      end
+      break
+    end
+  end
+end
 
+add_another_loop
+
+# ruby Project_Ateam.rb
 # !!!!!!!!!!!!!!!!!!!!!Beginning of code!!!!!!!!!!!!!!!!!!!!!
 # !!!!!!!!!!!!!!!!!!!!!Beginning of code!!!!!!!!!!!!!!!!!!!!!
 # !!!!!!!!!!!!!!!!!!!!!Beginning of code!!!!!!!!!!!!!!!!!!!!!
