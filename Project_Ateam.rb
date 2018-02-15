4bffc93d85e93bb386117dc438a74bde87e991d0
Tiago Soares <tiagosoaresjersey@gmail.com>
2018-02-15 19:12:58 +0000

    Test

---
+++ Project_Ateam.rb
@@ -0,0 +1,21 @@
+# !!!!!!!!!!!!!!!!!!!!!Space!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!Space!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!Space!!!!!!!!!!!!!!!!!!!!!
+# 1 Space
+def space1
+  puts ""
+end
+# 3 Spaces
+def space3
+  3.times {puts ""}
+end
+# 5 Spaces
+def space5
+  5.times {puts ""}
+end
+
+
+# !!!!!!!!!!!!!!!!!!!!!Beginning of code!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!Beginning of code!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!Beginning of code!!!!!!!!!!!!!!!!!!!!!
+space3
